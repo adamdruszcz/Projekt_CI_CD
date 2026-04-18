af25e37b66ffbaf604649d4bf459fbf08e8225c1
adamdruszcz <adam.druszcz@gmail.com>
2026-04-18 10:37:43 +0200

    time add

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import  Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Czesć Merito!"
+    return f"Czesć Merito! Mamy czas: {datetime.now()}"
 
 if __name__ == '__main__':
     app.run(debug=True)
